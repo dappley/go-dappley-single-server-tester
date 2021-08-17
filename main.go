dbb4633361f4a1610f9e7f4801feb7fc52d75403
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-08-17 13:40:50 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main 
 
 import (
+	"log"
 	"fmt"
 	"flag"
+	"errors"
 )
 
 func main() {
 	var function, senderEmail, senderPasswd string
 	flag.StringVar(&function, "function", "default_function", "Name of the function that will be run.")
-	flag.StringVar(&senderEmail,  "senderEmail",  "default_email@example.com", "Email of the addressee.")
+	flag.StringVar(&senderEmail,  "senderEmail",  "default_email", "Email of the addressee.")
 	flag.StringVar(&senderPasswd, "senderPasswd", "default_password", "Email password of the addressee.")
 	flag.Parse()
+
+	err := checkFlags(function, senderEmail, senderPasswd)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	if function == "update" {
 		update_host()
@@ -21,6 +29,21 @@ func main() {
 	} else if function == "terminate" {
 		terminate_host()
 	} else {
-		fmt.Println("Function Invalid!\nFunction can be one of below:\nupdate\ninitialize\nssh_command\nterminate")
+		fmt.Println("Error: Function is invalid!\nFunction can be one of below:\nupdate\ninitialize\nssh_command\nterminate")
 	}
+}
+
+//----------Helper----------
+func checkFlags(function string, email string, password string) (err error) {
+	switch {
+	case function == "default_function":
+		err = errors.New("Error: Function is missing!")
+	case email == "default_email":
+		err = errors.New("Error: Email is missing!")
+	case password == "default_password":
+		err = errors.New("Error: Password is missing!")
+	default:
+		err = nil
+	}
+	return err
 }
